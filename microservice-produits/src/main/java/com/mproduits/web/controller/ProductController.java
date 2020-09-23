3135a417ffac7b6cd26823cf29491bcc127ed256
Abdessamad MOUZAKKI <amouzakki@asi.fr>
2020-09-23 17:58:58 +0200

    :heavy_check_mark: Outsource microservices configuration

--- microservice-produits/src/main/java/com/mproduits/web/controller/ProductController.java
+++ microservice-produits/src/main/java/com/mproduits/web/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.mproduits.web.controller;
 
+import com.mproduits.configurations.ApplicationPropertiesConfiguration;
 import com.mproduits.dao.ProductDao;
 import com.mproduits.model.Product;
 import com.mproduits.web.exceptions.ProductNotFoundException;
@@ -16,16 +17,18 @@ public class ProductController {
 
     @Autowired
     ProductDao productDao;
+    @Autowired
+    ApplicationPropertiesConfiguration appProperties;
 
     // Affiche la liste de tous les produits disponibles
     @GetMapping(value = "/Produits")
     public List<Product> listeDesProduits() {
 
         List<Product> products = productDao.findAll();
-
         if (products.isEmpty()) throw new ProductNotFoundException("Aucun produit n'est disponible à la vente");
-        return products;
 
+        List<Product> limitProductsList = products.subList(0, appProperties.getLimitDeProduits());
+        return limitProductsList;
     }
 
     //Récuperer un produit par son id
